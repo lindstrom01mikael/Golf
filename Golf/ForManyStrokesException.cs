2630a5af0da400ea7fa79324545c4631f0aebd54
Mikael Lindström <mikael.lindstrom@lindstromsmanagement.se>
2023-11-08 22:19:47 +0100

    Add customized exceptions in this game

---
+++ Golf/ForManyStrokesException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Golf
+{
+	internal class ForManyStrokesException : Exception
+	{
+		public ForManyStrokesException(string message) : base(message)
+		{ }
+	}
+}
---
+++ Golf/OutOfBoundsException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Golf
+{
+	internal class OutOfBoundsException : Exception
+	{
+		public OutOfBoundsException(string message) : base(message)
+		{ }
+	}
+}
