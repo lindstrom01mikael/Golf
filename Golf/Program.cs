ba39a7df04ac835e3a260a7999d141b25f8f2952
Mikael Lindström <mikael.lindstrom@lindstromsmanagement.se>
2023-11-09 00:06:30 +0100

    Add variables for the game

--- Golf/Program.cs
+++ Golf/Program.cs
@@ -11,6 +11,35 @@ namespace Golf
 		/// The name of this program
 		/// </summary>
 		public const string ProgramName = "The Golfswing 2000";
+
+		// Declear a status property for this game
+		public bool IsRun { get; set; }
+
+		// Declear some field for this program/game
+		const double GRAVITY = 9.8;
+		static double initLocationToGoal, courseRange, distanceToGoal, angle;
+		static double velocity;
+		static double[] start, distance, end;
+		static int stroke;
+		static string strClubs;
+
+		static Random random = new Random();
+
+		// Declear the clubs that user had choise between
+		enum Golfclubs
+		{
+			I2 = 72,
+			I3 = 69,
+			I4 = 66,
+			I5 = 63,
+			I6 = 59,
+			I7 = 55,
+			I8 = 53,
+			I9 = 49,
+			PW = 45,
+			SW = 38,
+			PUTTER = 3
+		}
 		
 		/// <summary>
 		/// The start point of this program / game
